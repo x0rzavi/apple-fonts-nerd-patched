46907f39067a20393fc123978862ba657e6880e1
Avishek Sen <x0rzavi@gmail.com>
2023-03-08 11:25:04 +0530

    Add sf_arabic too

--- fonts.sh
+++ fonts.sh
@@ -18,49 +18,57 @@ apple_fonts () {
 
 	sf_pro () {
 		aria2c -x16 "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg"
-		7z x "SF-Pro.dmg"
+		7z x 'SF-Pro.dmg'
 		cd SFProFonts
 		7z x 'SF Pro Fonts.pkg'
 		7z x 'Payload~'
 		mv Library/Fonts/* $directory/tmpdir/AppleFontsNerdPatched
 		cd ..
 	}
+	sf_arabic () {
+		aria2c -x16 "https://devimages-cdn.apple.com/design/resources/download/SF-Arabic.dmg"
+		7z x 'SF-Arabic.dmg'
+    	cd SFArabicFonts
+    	7z x 'SF Arabic Fonts.pkg'
+    	7z x 'Payload~'
+		mv Library/Fonts/* $directory/tmpdir/AppleFontsNerdPatched
+		cd ..
+	}
 	sf_compact () {
 		aria2c -x16 "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg"
-		7z x "SF-Compact.dmg"
+		7z x 'SF-Compact.dmg'
 		cd SFCompactFonts
 		7z x 'SF Compact Fonts.pkg'
 		7z x 'Payload~'
 		mv Library/Fonts/* $directory/tmpdir/AppleFontsNerdPatched
 		cd ..
-	}	
-	sf_mono () {
-		aria2c -x16 "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg"
-		7z x "SF-Mono.dmg"
-		cd SFMonoFonts
-		7z x 'SF Mono Fonts.pkg'
-		7z x 'Payload~'
-		mv Library/Fonts/* $directory/tmpdir/AppleFonts
-		cd ..
 	}
 	ny () {
 		aria2c -x16 "https://devimages-cdn.apple.com/design/resources/download/NY.dmg"
-		7z x "NY.dmg"
+		7z x 'NY.dmg'
 		cd NYFonts
 		7z x 'NY Fonts.pkg'
 		7z x 'Payload~'
 		mv Library/Fonts/* $directory/tmpdir/AppleFontsNerdPatched
 		cd ..	
 	}
+	sf_mono () {
+		aria2c -x16 "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg"
+		7z x 'SF-Mono.dmg'
+		cd SFMonoFonts
+		7z x 'SF Mono Fonts.pkg'
+		7z x 'Payload~'
+		mv Library/Fonts/* $directory/tmpdir/AppleFonts
+		cd ..
+	}
 
 	sf_pro		# no nerd patches
+	sf_arabic	# no nerd patches
 	sf_compact	# no nerd patches
+	ny			# no nerd patches
 	sf_mono		# nerd patched
-	ny		# no nerd patches
 	
-	set +e
-	docker run --rm -v $directory/tmpdir/AppleFonts:/in -v $directory/tmpdir/AppleFontsNerdPatched:/out nerdfonts/patcher --no-progressbars --quiet
-	set -e
+	set +e && docker run --rm -v $directory/tmpdir/AppleFonts:/in -v $directory/tmpdir/AppleFontsNerdPatched:/out nerdfonts/patcher --no-progressbars --quiet && set -e
 	7z a $directory/AppleFontsNerdPatched.7z $directory/tmpdir/AppleFontsNerdPatched
 }
 
